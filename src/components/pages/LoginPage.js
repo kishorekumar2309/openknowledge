567f8a5e890766048c242acfd19f574a9d27ea5e
kishorekumar2309 <kishoreanandkumar@gmail.com>
2018-09-30 19:31:01 -0400

    Reset password

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import LoginForm from "../forms/LoginForm";
 import { login } from "../../actions/auth";
 
@@ -13,6 +14,7 @@ class LoginPage extends React.Component {
       <div>
         <h1>Login page</h1>
         <LoginForm submit={this.submit} />
+        <Link to="/forgot_password">Forgot Password?</Link>
       </div>
     );
   }
